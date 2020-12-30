aff76f9c163d0995987eef63c07381d7e8fda468
Dmi-991 <76758824+Dmi-991@users.noreply.github.com>
2020-12-30 18:34:39 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,123 @@
+#YangRecodeAnakHarqmAmin
+#Semoga Yang Recode Anak Anjing
+#Dasar Kontol
+Ba=banner.flf.
+Bu=bubble.flf.
+Le=lean.flf.....
+Sc=script.flf
+Sma=small.flf...
+Sms=smslant.flf...
+Di=digital.flf.
+Mi=mini.flf.....
+Sh=shadow.flf.
+Smsc=smscript.flf.
+St=standard.flf
+Iv=ivrit.flf...
+Mn=mnemonic.flf.
+Sl=slant.flf.
+Sm=smshadow.flf.
+Te=term.flf
+Bi=big.flf
+Bl=block.flf
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+
+clear
+sleep 1
+figlet -f $Ba "SELAMAT DATANG"
+sleep 1
+echo $cy 'Sebelum Login Baca Al-Fatiha'
+sleep 1
+echo $me 'Jika Kamu Kristen Baca Doa Sendiri...'
+echo $i 'WAKTU DIKASIH UNTUK MEMBACA DOA'
+Sleep 1
+echo $bi "WAKTU 05 Detik.."
+sleep 5
+echo $pu "SEMOGA WORK!!!..."
+sleep 2
+
+
+clear
+sleep 1.20
+echo "
+       ▄▄▄▄▄▄▄▄
+  █   ▄██████████▄
+ █▐   ████████████
+ ▌▐  ██▄▀██████▀▄██
+▐┼▐  ██▄▄▄▄██▄▄▄▄██
+▐┼▐  ██████████████
+▐▄▐████─▀▐▐▀█─█─▌▐██▄
+  █████──────────▐███▌
+  █▀▀██▄█─▄───▐─▄███▀
+  █  ███████▄██████ [•] Admin   : Mr.Riski
+     ██████████████ [•] Facebook: Fb.Com/lontong991
+     █████████▐▌██▌ [•] Versi   : New 1.12.1
+     ▐▀▐ ▌▀█▀ ▐ █    [°] Github. : Dmi-991
+           ▐    ▌
+╔═══════════════════════════════════════════╗
+    SpamNew-Sms Gratis      V 1.2.1
+╚═══════════════════════════════════════════╝
+" | lolcat
+sleep 1.26
+figlet -f $Ba "DMI-991"
+
+echo $cy "Sebelum Masuk Masukan Id Termux"
+sleep 1
+echo -n "\033[1;33m[*]===[=SpamNew==]=>>>\033[1;37m "
+
+read Asw
+case $Asw in
+DumaiSST) echo $me "ID Yang Anda Masukan Benar!!!"
+sleep 1.26
+echo $ku 'Silahkan Masukan Passwordnya'
+sleep 1
+echo -n "\033[1;33m[*]===[=SpamNew==]=>>>\033[1;37m "
+
+read Pw
+case $Pw in
+Riski) echo $pu 'PASSWORD YANG' $cy "Ada Masukan Benar...."
+sleep 1
+echo $me 'INSTALL BAHAN TERMUX'
+sleep 1
+echo $ku "DIMulai!!!!!"
+sleep 2
+clear
+
+apt update && apt upgrade
+pkg ins toilet -y
+pkg ins figlet -y
+pkg ins ruby -y
+pkg ins gem -y
+gem ins lolcat -y
+pkg ins php -y
+pkg ins python2 -y
+pkg ins git -y
+pkg update && pkg upgrade -y
+pkg install python2 -y
+pkg install git -y
+pkg install php -y
+pip2 install requests -y
+pip2 install mechanize -y
+pip install lolcat -y
+echo $cy "Install Bahan Sukses..."
+clear
+sleep 3
+python SpamNew.py
+if
+
+
+
+
+**)echo $ku "Jika Tidak Punya Id Dan Password Chat Admin"
+sleep 1
+echo $me "082384332714 Name Admin RISKI"
+sleep 5.10
+sh install.sh
+if
