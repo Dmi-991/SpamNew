4ac8972016f0a869ffec062e277305dd45b8302d
Dmi-991 <76758824+Dmi-991@users.noreply.github.com>
2020-12-31 03:49:22 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,168 @@
+#TesBuatScriptLogin25-Desember-2020
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+blk=$'\x1b[90m' # Sets text to black
+red=$'\x1b[31m' # Sets text to red
+grn=$'\x1b[92m' # Sets text to green
+ylw=$'\x1b[93m' # Sets text to yellow
+blu=$'\x1b[94m' # Sets text to blue
+pur=$'\x1b[95m' # Sets text to purple
+cyn=$'\x1b[96m' # Sets text to cyan
+wht=$'\x1b[97m' # Sets text to white
+rst=$'\x1b[0m'  # resets to default terminal color
+
+Bi='\033[34;1m'
+Hi='\033[32;1m'
+Pu='\033[35;1m'
+Cy='\033[36;1m'
+Re='\033[31;1m'
+Pu='\033[37;1m'
+Ku='\033[33;1m'
+sleep 1
+
+BL='\033[0;30m'
+BLU='\033[0;34m'
+GR='\033[0;32m'
+Ca='\033[0;36m'
+Re='\033[0;31m'
+PU='\033[0;35m'
+B='\033[0;33m'
+LG='\033[0;37m'
+Dg='\033[1;30m'
+G='\033[1;32m'
+F='\033[1;36m'
+D='\033[1;31m'
+P='\033[1;35m'
+R='\033[1;33m'
+K='\033[1;37m'
+C='\033[1;33m'
+clear
+
+#YangRecodeAnakHarqmAmin
+#Semoga Yang Recode Anak Anjing
+#Dasar Kontol
+Ba=banner.flf
+Bu=bubble.flf.
+Le=lean.flf.....
+Sc=script.flf
+Sma=small.flf...
+Sms=smslant.flf...
+Di=digital.flf.
+Mi=mini.flf.....
+Sh=shadow.flf.
+GNU nano 5.4                  install.sh                  Modified
+Te=term.flf
+Bi=big.flf
+Bl=block.flf
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+
+clear
+sleep 1
+figlet -f $Ba "WELCOME"
+sleep 1
+echo $cy 'Sebelum Login Baca Al-Fatiha'
+sleep 1
+echo $me 'Jika Kamu Kristen Baca Doa Sendiri...'
+sleep 1
+echo $i 'WAKTU DIKASIH UNTUK MEMBACA DOA'
+sleep 1
+echo $bi "WAKTU 05 Detik.."
+sleep 5
+echo $pu "SEMOGA WORK!!!..."
+sleep 2
+
+clear
+echo "
+       ▄▄▄▄▄▄▄▄
+  █   ▄██████████▄
+ █▐   ████████████
+ ▌▐  ██▄▀██████▀▄██
+▐┼▐  ██▄▄▄▄██▄▄▄▄██
+▐┼▐  ██████████████
+▐▄▐████─▀▐▐▀█─█─▌▐██▄
+  █████──────────▐███▌
+  █▀▀██▄█─▄───▐─▄███▀
+  █  ███████▄██████ [•] Admin   : Mr.Riski
+     ██████████████ [•] Facebook: Fb.Com/lontong991
+     █████████▐▌██▌ [•] Versi   : New 1.12.1
+     ▐▀▐ ▌▀█▀ ▐ █   [•] Github  : Dmi-991
+           ▐    ▌
+╔═══════════════════════════════════════════╗
+    SpamNew Ultimitit    V 1.12.1
+╚═══════════════════════════════════════════╝
+" | lolcat
+
+figlet -f $Ba "SpamNew"
+echo $cy "Masukan Pin Termux Untuk Login.."
+sleep 1
+echo ""
+sleep 1
+echo -n "\033[1;33m[*]XX[=PIN+TERMUX==]=>>>\033[1;37m "
+sleep 1
+read Asw
+case $Asw in
+9912020) echo "
+Pin Yang Anda Masukan Benar....!!
+Sebelum Lanjut.... Bahan Harus DiInstqll..
+"
+sleep
+echo $ku "Orang Yang Sabar Di" $pu "Sayqng Tuhan.."
+sleep 2
+echo $cy "Mulai Instqll...!!"
+#Bahqn
+pkg update && pkg upgrade -y
+pkg install python2 -y
+pkg install git -y
+pkg install php -y
+pip2 install requests -y
+pip2 install mechanize -y
+pip install lolcat -y
+apt update && apt upgrade -y
+pkg ins toilet -y
+pkg ins figlet -y
+pkg ins ruby -y
+pkg ins gem -y
+gem ins lolcat -y
+pkg ins php -y
+pkg ins python2 -y
+pkg ins git -y
+clear
+sleep 1
+echo $bi "Install Bahan Sudah Selesai..."
+sleep 1
+
+
+
+
+;;
+
+**) echo $cy "Jangan Bodohlah Anjing...."
+sleep 1
+echo $me "Apakah Anda Tidak Mempunyai PIN Termux"
+sleep 1
+echo $i "Silahkan Chat Admin 082384332714"
+sleep 1
+echo $bi "Chat Admin Jika Malu PIN Termux..."
+sleep 3
+sleep 1
+sh install.sh
+
+
+esac
+done
+done
